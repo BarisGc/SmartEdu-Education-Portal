3e5d5618c90f5a70bb22a13422b66e30bdc7667e
BarisGc <baris.devrim1@gmail.com>
2022-03-01 17:04:37 +0300

    login/logout final

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const pageRoute = require('./routes/pageRoute')
 const courseRoute = require('./routes/courseRoute')
 const categoryRoute = require('./routes/categoryRoute')
@@ -18,12 +21,28 @@ mongoose.connect('mongodb://localhost/smart-edu-db', {
 // Template Engine
 app.set("view engine", "ejs");
 
+// Global Variable
+global.userIN = null;
+
 // Middlewares
+
 app.use(express.static("public"));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(
+    session({
+        secret: 'my_keyboard_cat', // Buradaki texti değiştireceğiz.
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smart-edu-db' }),
+    })
+);
 
 // Routes
+app.use('*', (req, res, next) => {
+    userIN = req.session.userID;
+    next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
