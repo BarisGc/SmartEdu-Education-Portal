179ced7485e2229b263086b1d1c38ca1a0596f55
BarisGc <baris.devrim1@gmail.com>
2022-03-02 18:39:51 +0300

    FlashMessages

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -31,8 +31,9 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-    const outputMessage =
-        `
+    try {
+        const outputMessage =
+            `
     <h1>Mail Details </h1>
     <ul>
         <li>Name: ${req.body.name}</li>
@@ -42,31 +43,36 @@ exports.sendEmail = async (req, res) => {
     <p>${req.body.message}</p>
         `
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: "baris.devrim01@gmail.com", // gmail user
-            pass: "mtxuuirhgdwrsrji", // gmail password
-        },
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: "baris.devrim01@gmail.com", // gmail user
+                pass: "mtxuuirhgdwrsrji", // gmail password
+            },
+        });
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Smart EDU Contact Form" <baris.devrim01@gmail.com>', // sender address
-        to: "baris.devrim1@gmail.com", // list of receivers
-        subject: "Smart EDU Contact Form New Message ✔", // Subject line
-        html: outputMessage, // html body
-    });
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Smart EDU Contact Form" <baris.devrim01@gmail.com>', // sender address
+            to: "baris.devrim1@gmail.com", // list of receivers
+            subject: "Smart EDU Contact Form New Message ✔", // Subject line
+            html: outputMessage, // html body
+        });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-    res.status(200).redirect('contact');
+        req.flash("success", "We Received your message succesfully");
+        res.status(200).redirect('contact');
+    } catch (err) {
+        req.flash("error", `Something happened! ${err}`);
+        res.status(200).redirect('contact');
+    }
 };
